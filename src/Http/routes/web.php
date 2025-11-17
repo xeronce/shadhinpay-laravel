55441baaa77dfec5613baf3202d90bf762de2f3a
TazimHome <tazim@home.com>
2025-11-17 17:48:24 +0600

    update route

--- src/Http/routes/web.php
+++ src/Http/routes/web.php
@@ -7,7 +7,7 @@ use Xeronce\Shadhinpay\Http\Controllers\WebhookController;
 
 Route::prefix('shadhinpay')->group(function () {
 
-    Route::post('/', [ShadhinPayController::class, 'connectionStatus']);
+    Route::get('/', [ShadhinPayController::class, 'connectionStatus']);
     Route::post('/pay', [PaymentController::class, 'create']);
     Route::get('/verify/{trxId}', [PaymentController::class, 'verify']);
 
